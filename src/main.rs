1b89630863e3be9d5666969a68d26d3c25143bc2
m0nsta0 <rubik.sammy@gmail.com>
2023-01-09 20:51:30 -0500

    almost init

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,15 @@ struct NeuralNetwork {
     layer3_biases: Vec<f64>,
 }
 
+fn init_nn() -> NeuralNetwork {
+    layer1_weights: vec![vec![]],
+    layer1_biases: Vec<f64>,
+    layer2_weights: Vec<Vec<f64>>,
+    layer2_biases: Vec<f64>,
+    layer3_weights: Vec<Vec<f64>>,
+    layer3_biases: Vec<f64>,
+}
+
 fn run_first_layer(weights: Vec<Vec<f64>>, biases: Vec<f64>, inputs: Vec<f64>) -> Vec<f64> {
     let mut out_list: Vec<f64> = vec![];
     for i in 0..weights.len() {
@@ -49,13 +58,15 @@ fn run_third_layer(weights: Vec<Vec<f64>>, biases: Vec<f64>, inputs: Vec<f64>) -
     out_list
 }
 fn run_full(nn: NeuralNetwork, input: Vec<f64>) -> Vec<f64> {
-    let output1 = run_first_layer(NeuralNetwork.layer1_weights, NeuralNetwork.layer1_biases, input);
-    vec![0.0]
+    let output1 = run_first_layer(nn.layer1_weights, nn.layer1_biases, input);
+    let output2 = run_second_layer(nn.layer2_weights, nn.layer2_biases, output1);
+    let output3 = run_third_layer(nn.layer3_weights, nn.layer3_biases, output2);
+    output3
 }
 fn sigmoid(input: f64) -> f64{
     1.0 / (1.0 + f64::powf(2.71828, input * -1.0))
 }
 
 fn main() {
-    
+    println!("{:#?}", run_full())
 }
